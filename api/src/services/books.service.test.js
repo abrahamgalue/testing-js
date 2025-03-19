86bfdd6dd2823dbc0faaf7c0dfad4536cc217aeb
Abraham Galue <abrahamgalue512@gmail.com>
2025-03-19 19:01:46 -0400

    Mocking MongoLib implementation

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,5 +1,19 @@
 const BooksService = require('./books.service');
 
+const fakesBooks = [
+  {
+    id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakesBooks],
+  create: () => { },
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
@@ -11,9 +25,16 @@ describe('Test for BooksService', () => {
       // GIVEN
       // WHEN
       const books = await service.getBooks({});
-
       // THEN
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(1);
+    });
+
+    test('should return a list book', async () => {
+      // GIVEN
+      // WHEN
+      const books = await service.getBooks({});
+      // THEN
+      expect(books[0].name).toEqual('Harry Potter');
     });
   });
 });
