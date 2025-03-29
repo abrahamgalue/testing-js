7acfab22b167fe4347ad8f9be460fe78524b8e46
Abraham Galue <abrahamgalue512@gmail.com>
2025-03-28 20:09:54 -0400

    Added e2e tests

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,39 +1,52 @@
-const createApp = require('../src/app')
-const { generateManyBooks } = require('../src/fakes/book.fake')
-
 const request = require('supertest')
+const { MongoClient } = require('mongodb')
 
-const mockGetAll = jest.fn()
+const createApp = require('../src/app')
+const { config } = require('../src/config')
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: () => mockGetAll(),
-  create: () => { },
-})))
+const DB_NAME = config.dbName
+const MONGO_URI = config.dbUrl
 
 describe('Test for books', () => {
   let app;
   let server;
-  beforeAll(() => {
+  let database;
+
+  beforeAll(async () => {
     app = createApp()
     server = app.listen(3002)
+    const client = new MongoClient(MONGO_URI)
+    await client.connect()
+    database = client.db(DB_NAME)
   })
 
   afterAll(async () => {
     await server.close()
+    await database.dropDatabase()
   })
 
   describe('test for [GET] /api/v1/books', () => {
     test('should return a list of books', async () => {
       // GIVEN
-      const fakeBooks = generateManyBooks(4)
-      mockGetAll.mockResolvedValue(fakeBooks)
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book 1',
+          year: 1998,
+          author: 'nicolas'
+        },
+        {
+          name: 'Book 2',
+          year: 2003,
+          author: 'pedro'
+        },
+      ])
       // WHEN
       return request(app)
         .get('/api/v1/books')
         .expect(200)
         .then(({ body }) => {
           // THEN
-          expect(body.length).toEqual(fakeBooks.length)
+          expect(body.length).toEqual(seedData.insertedCount)
         })
     })
   })
